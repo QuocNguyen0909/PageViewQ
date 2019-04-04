2f1864d1a990db6821845479e0ba30bcac18a2a1
QUỐC <quoc@QUOCs-MacBook-Pro.local>
2019-04-04 22:42:10 +0700

    Inital commit

---
+++ PagViewQV/PagViewQV.h
@@ -0,0 +1,19 @@
+//
+//  PagViewQV.h
+//  PagViewQV
+//
+//  Created by QUỐC on 4/4/19.
+//  Copyright © 2019 QUỐC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagViewQV.
+FOUNDATION_EXPORT double PagViewQVVersionNumber;
+
+//! Project version string for PagViewQV.
+FOUNDATION_EXPORT const unsigned char PagViewQVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagViewQV/PublicHeader.h>
+
+
